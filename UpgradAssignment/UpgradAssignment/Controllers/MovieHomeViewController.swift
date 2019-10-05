9bf7716119e31ee23694853284b3fe12e8741a3e
vinit somani <vinitsomani07@gmail.com>
2019-10-05 16:21:30 +0530

    HomeViewController UI setup.

---
+++ UpgradAssignment/UpgradAssignment/Controllers/MovieHomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieHomeViewController.swift
+//  UpgradAssignment
+//
+//  Created by AiTechnology on 10/5/19.
+//  Copyright © 2019 vinit.somani. All rights reserved.
+//
+
+import UIKit
+
+class MovieHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ UpgradAssignment/UpgradAssignment/Views/MovieCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCollectionViewCell.swift
+//  UpgradAssignment
+//
+//  Created by AiTechnology on 10/5/19.
+//  Copyright © 2019 vinit.somani. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
